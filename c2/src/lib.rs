e23d46c602efb662f2dcc0549546c66f7494df4b
Logan Boyd <logan.boyd@protonmail.com>
2026-03-21 16:43:50 -0700

    Remove lowering for now
    ehh, not ready

--- c2/src/lib.rs
+++ c2/src/lib.rs
@@ -2,4 +2,3 @@ pub mod check;
 pub mod component;
 pub mod front;
 pub mod ir;
-pub mod lower;
--- c2/src/lower/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod component_to_semantic;
-pub mod semantic_to_shapes;
-
-pub use component_to_semantic::lower_component_to_semantic;
-pub use semantic_to_shapes::lower_semantic_to_shape_data;
