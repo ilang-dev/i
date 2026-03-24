8d2068537ca44722d4d213fc9effd905f0fd6f90
Logan Boyd <logan.boyd@protonmail.com>
2026-03-23 17:19:05 -0700

    Add ir/stage.rs

--- c2/src/ir/stage.rs
+++ c2/src/ir/stage.rs
@@ -1,5 +1,11 @@
 use super::common::Op;
 
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct ScheduledStage {
+    stage: Stage,
+    schedule: Schedule,
+}
+
 /// Expression-local semantic content of one 𝚒 expression.
 ///
 /// A `Stage` consists of a scalar op, a local iteration rank, one explicit
@@ -28,3 +34,29 @@ pub struct Axis(pub usize);
 /// dimension order.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Index(pub Vec<Axis>);
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Schedule {
+    splits: Vec<SplitList>, // assert!(splits.len() == stage.rank)
+    order: Vec<AxisRef>, // must represent every loop exactly once, where part 0 is the base loop and valid parts for axis a are 0..=splits[a].0.len(); assert!(order.len() == splits.iter().map(|x| x.0.len() + 1).sum())
+    compute_sites: Vec<Option<Site>>, // per input, assert!(compute_sites.len() == stage.inputs.len())
+    init_site: Option<Site>, // reduction init site; for pointwise stages assert!(init_site.is_none())
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct SplitList(Vec<SplitFactor>);
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct SplitFactor(usize);
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Site {
+    Root,
+    At(AxisRef),
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct AxisRef {
+    pub axis: Axis,
+    pub part: usize,
+}
