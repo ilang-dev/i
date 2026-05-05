2dee2658304738df350b9c9ebb35a1136e83c42c
Logan Boyd <logan.boyd@protonmail.com>
2026-05-04 17:40:16 -0700

    Remove dead code from ir/common.rs

--- c2/src/ir/common.rs
+++ c2/src/ir/common.rs
@@ -1,10 +1,4 @@
 pub type Axis = char;
-pub type ExprId = usize;
-pub type ValueId = usize;
-pub type StageId = usize;
-pub type KernelId = usize;
-pub type BufferId = usize;
-pub type LoopId = usize;
 
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Index(pub usize);
@@ -26,42 +20,6 @@ pub struct DimRef<B> {
 pub struct Extent<B> {
     pub source: DimRef<B>,
     pub kind: ExtentKind,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub enum StaticExtent {
-    Known(usize),
-    Param(String),
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub enum ExtentSource {
-    Const(usize),
-    InputDim { input: usize, dim: usize },
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Shape(pub Vec<StaticExtent>);
-
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub enum Scalar {
-    Float,
-    Int,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct TensorType {
-    pub scalar: Scalar,
-    pub shape: Shape,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Pattern(pub Vec<Axis>);
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Split {
-    pub axis: Axis,
-    pub factors: Vec<StaticExtent>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
